8ff44bf724fa150cb48a2a8fdf19b223ed9e34ea
Munesh Sutar <85051705+muneshsutar@users.noreply.github.com>
2022-02-24 15:27:19 +0530

    fast
    fast

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect, useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Router, Switch, Route, Link } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+// custom component
+import { Header } from "./components/Header";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+
+
+
+
+import { logout } from "./actions/auth";
+import { clearMessage } from "./actions/message";
+
+import { history } from "./helpers/history";
+
+// import AuthVerify from "./common/AuthVerify";
+import EventBus from "./common/EventBus";
+
+const App = () => {
+  const [showModeratorBoard, setShowModeratorBoard] = useState(false);
+  const [showAdminBoard, setShowAdminBoard] = useState(false);
+
+  const { user: currentUser } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    history.listen((location) => {
+      dispatch(clearMessage()); // clear message when changing location
+    });
+  }, [dispatch]);
+
+  const logOut = useCallback(() => {
+    dispatch(logout());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (currentUser) {
+      setShowModeratorBoard(currentUser.roles.includes("ROLE_MODERATOR"));
+      setShowAdminBoard(currentUser.roles.includes("ROLE_ADMIN"));
+    } else {
+      setShowModeratorBoard(false);
+      setShowAdminBoard(false);
+    }
+
+    EventBus.on("logout", () => {
+      logOut();
+    });
+
+    return () => {
+      EventBus.remove("logout");
+    };
+  }, [currentUser, logOut]);
+
+  return (
+    <Router history={history}>
+      <div>
+        <Header />
+        <div className="container mt-3">
+          <Switch>
+            <Route exact path={["/", "/home"]} component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/profile" component={Profile} />
+            </Switch>
+            </div>
+
+        {/* <AuthVerify logOut={logOut}/> */}
+      </div>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/components/Body/ViewStudentComponent.jsx
@@ -0,0 +1,106 @@
+import React, { Component } from "react";
+import StudentService from '../../services/StudentService'
+import { BrowserRouter as Link } from "react-router-dom";
+
+import { IoMan, IoBus } from "react-icons/io5";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardTitle,
+  CardText,
+  CardFooter,
+} from "reactstrap";
+
+import { Col, Row, Container } from "reactstrap";
+
+class ViewStudentComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      id: this.props.match.params.id,
+      student: {},
+    };
+  }
+
+  componentDidMount() {
+    StudentService.getStudentById(this.state.id).then((res) => {
+      this.setState({ student: res.data });
+    });
+  }
+
+  render() {
+    // renderStudent(students) {
+    return (
+      <Container>
+        
+        <Container className="mt-4">
+          <Row>
+            <Col sm="3">
+              <Button block color="success" onClick={this.addStudent}>
+              <a className="text-white" href="/students"> <img src="https://img.icons8.com/metro/19/ffffff/back.png"/>Back</a>
+              </Button>
+            </Col>
+          </Row>
+        </Container>
+
+        <Row>
+          <Col sm="12">
+            <Card body>
+              <CardTitle tag="h5">
+                <IoMan className="font-size-xl" />
+                <span>{this.state.student.name}</span>
+              </CardTitle>
+              <CardBody>
+                <Row>
+                  <Col sm="4" className="text-center">
+                    <span className="font-weight-bold">
+                      {" "}
+                      Student Registration Number:
+                    </span>
+                    <span>{ this.state.student.regdNo }</span>
+                  </Col>
+
+                  <Col sm="4" className="text-center">
+                    <span className="font-weiht-bolder">
+                    Student Email ID:
+                    </span>
+                    <span>{ this.state.student.emailId }</span>
+                  </Col>
+
+                  <Col sm="4" className="text-center">
+                    <span className="font-weith-bold">
+                    Student Address Name:
+                    </span>
+                    <span>{ this.state.student.address }</span>
+                  </Col>
+
+                  <Col sm="4" className="text-center">
+                    <span className="font-weith-bold">Student PicUp Address Name:</span>
+                    <span>{ this.state.student.pickUpAddress }</span>
+                  </Col>
+
+                  <Col sm="4" className="text-center">
+                    <span className="font-weith-bold">Student Mobile Number:</span>
+                    <span>{ this.state.student.mobileNo }</span>
+                  </Col>
+
+                  <Col sm="4" className="text-center">
+                    <span className="font-weith-bold">
+                    Student Parance Mobile Number: 
+                    </span>
+                    <span>{ this.state.student.parance_mobileNo }</span>
+                  </Col>
+                </Row>
+              </CardBody>
+              
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default ViewStudentComponent;
